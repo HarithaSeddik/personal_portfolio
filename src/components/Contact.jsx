dbcb4f00688a6d4807f82c160ee7fc54de541279
Harisa Seddik <harisa.seddik@kobil.com>
2023-09-12 20:19:24 +0300

    update contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,13 +18,18 @@ const Contact = () => {
     email: "",
     message: "",
   });
+  const [sendDisabled, setSendDisabled] = useState(true);
 
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     const { target } = e;
     const { name, value } = target;
-
+    if (form.email && form.message && form.name) {
+      setSendDisabled(false);
+    } else {
+      setSendDisabled(true);
+    }
     setForm({
       ...form,
       [name]: value,
@@ -60,7 +65,7 @@ const Contact = () => {
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+        className="flex-[0.65] bg-black-100 p-8 rounded-2xl"
       >
         <p className={styles.sectionSubText}>
           Want to discuss a project or just chat? Just leave me a message!
@@ -70,7 +75,7 @@ const Contact = () => {
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"
+          className="mt-8 flex flex-col gap-5"
         >
           <label className="flex flex-col">
             <span className="text-white font-medium mb-4">Your Name</span>
@@ -80,7 +85,7 @@ const Contact = () => {
               value={form.name}
               onChange={handleChange}
               placeholder="Name*"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-light"
             />
           </label>
           <label className="flex flex-col">
@@ -91,24 +96,25 @@ const Contact = () => {
               value={form.email}
               onChange={handleChange}
               placeholder="Email Address*"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-light"
             />
           </label>
           <label className="flex flex-col">
             <span className="text-white font-medium mb-4">Your Message</span>
             <textarea
-              rows={7}
+              rows={4}
               name="message"
               value={form.message}
               onChange={handleChange}
               placeholder="What you want to say?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-light"
             />
           </label>
 
           <button
             type="submit"
-            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+            className="bg-secondaryBg py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary disabled:opacity-25"
+            disabled={sendDisabled}
           >
             {loading ? "Sending..." : "Send"}
           </button>
